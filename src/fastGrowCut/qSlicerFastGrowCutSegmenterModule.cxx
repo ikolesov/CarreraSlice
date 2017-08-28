705da266ed36a04cb97e2b2fe8108929da597ee4
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2017-08-28 01:49:01 -0400

    COMP: Qt5: Fix error: static assertion failed: Old plugin system used

--- src/fastGrowCut/qSlicerFastGrowCutSegmenterModule.cxx
+++ src/fastGrowCut/qSlicerFastGrowCutSegmenterModule.cxx
@@ -25,7 +25,10 @@
 #include "qSlicerFastGrowCutSegmenterModule.h"
 
 //-----------------------------------------------------------------------------
+#if (QT_VERSION < QT_VERSION_CHECK(5, 0, 0))
+#include <QtPlugin>
 Q_EXPORT_PLUGIN2(qSlicerFastGrowCutSegmenterModule, qSlicerFastGrowCutSegmenterModule);
+#endif
 
 //-----------------------------------------------------------------------------
 /// \ingroup Slicer_QtModules_ExtensionTemplate
